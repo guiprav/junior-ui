cdef6049ddb653eb5e4bb4cda0c256ebf10aacc0
Gui Prá <super.driver.512@gmail.com>
2018-09-29 01:39:28 -0300

    Build fixup.

--- bundle.js
+++ bundle.js
@@ -281,6 +281,8 @@ jr.init = () => {
     }
 
     for (let [el, attrs] of dirtyAttrs.entries()) {
+      jr.initEl(el);
+
       let indexEntry = jr.index.get(el);
       let indexAttrs = indexEntry.attrs;
 
